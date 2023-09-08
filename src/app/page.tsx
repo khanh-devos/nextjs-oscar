f3487266694478d39fd86b7b02bc729dc1bc46dd
khanh-devos <dreamproperty.khanh@gmail.com>
2023-09-08 14:53:32 +0700

    config SSR and CSR - SSR: fetching API to take the list of Nobelists. - Created and render Nation and NationItem at CSR. - Implemented Images inside NationItem.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,51 @@
+import Nation from '../components/Nation';
+import { getNobelists } from './ssr';
 
-export default function Home() {
+
+export type uniCountry = {
+  id: Number,
+  country: String,
+  cities: Array<String>
+}
+
+export type Nobel = {
+  id: number, 
+  fullname: string, 
+  gender: string, 
+  date: string, 
+  city: string, 
+  country: string, 
+  awardYear: string, 
+  category: string, 
+  prize: string,
+}
+
+
+const takeUniqueCountries = (arr: any) => {
+  const uniqueCountries: Array<uniCountry> = [];
+  const compare: Array<string> = [];
+
+  arr.forEach(({ country, id, city }: any) => {
+    if (!compare.includes(country)) {
+      compare.push(country);
+      uniqueCountries.push({ id, country, cities: [city] });
+    } else {
+      const check: any = uniqueCountries.find((item) => item.country === country);
+      check.cities.push(city);
+    }
+  });
+  return uniqueCountries;
+};
+
+
+export default async function Home() {
+  const nobelists: Array<Nobel> = await getNobelists();
+  const uniqueCountries: Array<uniCountry> = takeUniqueCountries(nobelists)
+
   return (
     <div>
       <h2>Home</h2>
+      <Nation nobelists={nobelists} uniqueCountries={uniqueCountries}/>
     </div>
   )
 }
---
+++ src/app/ssr.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const API_NOBEL = 'https://api.nobelprize.org/2.1/laureates';
+
+
+function filterData (data: any)  {
+  const res = data.laureates.map((item: any) => {
+    const { id, fullName: { en: fullname }, gender } = item;
+    const { date } = item.birth;
+    const { en: city } = item.birth.place?.cityNow || { en: 'Unknown' };
+    const { en: country } = item.birth.place?.countryNow || { en: 'Unknown' };
+
+    const {
+      awardYear,
+      categoryFullName: { en: category },
+      prizeAmount: prize,
+    } = item.nobelPrizes[0];
+
+    return {
+      id, fullname, gender, date, city, country, awardYear, category, prize,
+    };
+  });
+
+  return res;  
+}
+
+
+export async function getNobelists () {
+  try {
+    const res = await axios.get(API_NOBEL);
+    return filterData(res.data);
+  } catch (err) {
+    return err
+  } 
+}
