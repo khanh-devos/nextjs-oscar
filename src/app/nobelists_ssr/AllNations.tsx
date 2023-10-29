1361ea38b9bf173a11d0bc8881304e8d1ec0004e
khanh-devos <dreamproperty.khanh@gmail.com>
2023-10-29 22:58:04 +0700

    again for Image

--- src/app/nobelists_ssr/AllNations.tsx
+++ src/app/nobelists_ssr/AllNations.tsx
@@ -2,9 +2,10 @@
 
 import IMAGES, { ArrowCircleRightIcon } from "@/container";
 import Link from "next/link";
-import { useContext } from "react";
 import { Nobel, uniCountry } from "./App";
 import MyHeader from "./Header";
+import Image from 'next/image';
+import { v4 } from "uuid";
 
 
 export default function Nations({
@@ -27,7 +28,8 @@ export default function Nations({
         uniqueCountries.map(({country, cities}, index:number) => {
           
           return (
-          <Link 
+          <Link
+            key={v4()}
             href={`/nobelists_ssr/${country}`} 
             className={`relative p-1 h-32 ${darken(index)}`} >
             
@@ -40,9 +42,11 @@ export default function Nations({
               <p className="text-sm">{cities.length}</p>
             </div>
             
-            <img 
+            <Image 
               src={IMAGES[country.toLowerCase().replace(' ', '')].src}
               alt="nation map" 
+              width={120}
+              height={120}
               className="h-24 min-h-24 p-3 m-auto opacity-25"
             />
           </Link>)}
--- src/app/nobelists_ssr/Header.tsx
+++ src/app/nobelists_ssr/Header.tsx
@@ -2,7 +2,7 @@
 
 import IMAGES, { BackIcon, MicroIcon, SettingIcon } from "@/container";
 import { useRouter } from "next/navigation";
-
+import Image from 'next/image';
 
 export default function MyHeader({
   title, country, amount, stats
@@ -48,10 +48,12 @@ export default function MyHeader({
       </div>
 
       <div className={"grid grid-cols-2 pt-2 pb-2 color-5"}>
-        <img
+        <Image
           className={"m-auto h-24 max-h-24 p-2 pr-0 opacity-30"}
           src={IMAGES[country.toLowerCase().replace(/[\d\W]/gi, '')].src}
           alt="nation map"
+          width={200}
+          height={200}
         />
         <div className={`grid grid-cols-1 gap-0 content-center p-5 ${styled()}`} >
           <h2 className="text-xl font-bold leading-5">{country.toUpperCase().replace("%20", ' ')}</h2>
