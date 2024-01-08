7c9e1f6d44bd628b736cbb8fc488a2129b7006ac
khanh-devos <dreamproperty.khanh@gmail.com>
2024-01-08 21:32:21 +0700

    Fix initial start at the Home page

--- src/app/portfolio/components/Home.tsx
+++ src/app/portfolio/components/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
 
     <MyParagraph1 text="A remote full-stack developer." />
     
-    <MyParagraph2 text='I am a motorbiker || e-biker. A cool wind while riding is a piece of my favor. I enjoy processing things with either JavaScript or TypeScript (React & Nextjs). I also know Rails on Ruby and AI (Python). If you need a remote developer, just email me "dreamproperty.khanh@gmail.com".' />
+    <MyParagraph2 text='I am a motorbiker || an e-biker. A cool wind while riding is a piece of my favor. I enjoy processing things with either JavaScript or TypeScript (React & Nextjs). I also know Rails on Ruby and AI (Python). If you need a remote developer, just email me "dreamproperty.khanh@gmail.com".' />
   </MyLinearGradient>
   </div>
   )
