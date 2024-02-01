7fb6a6a5ddac688c3d21f25e932736d7df6d1ffe
khanh-devos <dreamproperty.khanh@gmail.com>
2024-02-01 22:27:37 +0700

    Re-compose self-introduction

--- src/app/portfolio/components/Home.tsx
+++ src/app/portfolio/components/Home.tsx
@@ -1,5 +1,5 @@
 import { MyHeader1, MyLinearGradient,
-  MyParagraph1, MyParagraph2 } from "@/app/styledComponents";
+  MyParagraph2 } from "@/app/styledComponents";
 import Reflection from "../reflection/Reflection";
 
 
@@ -16,15 +16,13 @@ const Home = () => {
       >
 
         <MyHeader1 text="Hi, I am Khanh." />
-
-        <MyParagraph1 text="A remote full-stack developer." />
       
-        <MyParagraph2 text='I am a motorbiker || an e-biker. A cool wind while riding is a piece of my favor. I enjoy processing things with either JavaScript or TypeScript (React & Nextjs). I also know Rails on Ruby and AI (Python). If you need a remote developer, just email me "dreamproperty.khanh@gmail.com".' />
+        <MyParagraph2 text='I am a remote full-stack developer || a motorbiker || an e-biker. A cool wind while riding is a piece of my favor. I enjoy coding either FE (React & Nextjs, TS & JS) or BE (Rails on Ruby, Python-AI).' />
 
         <MyParagraph2 text='P/S:' />
-        <MyParagraph2 text='- This responsive portfolio was written with Nextjs & Typescript.' />
-        <MyParagraph2 text='- Extra responsive fading-flying feature added in React Carousel in the page "Projects".' />
-        <MyParagraph2 text='- The background image was made by Typograms.' />
+        <MyParagraph2 text='- Written with Nextjs & Typescript.' />
+        <MyParagraph2 text='- 4 features added : responsive, sliding, fading, and flying.' />
+        <MyParagraph2 text='- Special feature implemented by my own library : REFLECTION.' />
       
         <br/>
 
