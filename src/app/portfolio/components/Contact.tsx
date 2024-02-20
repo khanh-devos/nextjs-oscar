a1c0f35c6540e6dfd68532e0460eae34a49ade19
khanh-devos <dreamproperty.khanh@gmail.com>
2024-02-20 12:25:32 +0700

    Adjust borderPathScale.

--- src/app/portfolio/components/Contact.tsx
+++ src/app/portfolio/components/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
   return (
   <div className="mt-20">
     <div style={{width: 'fit-content', margin: 'auto'}}>
-      <Reflection border={true} borderWidth={9} borderColor="skyblue" borderRadiusRatio={1.2} borderPathScale={2} light={false} borderRadius="20px">
+      <Reflection border={true} borderWidth={9} borderColor="skyblue" borderRadiusRatio={1} borderPathScale={2.5} light={false} borderRadius="20px">
         <MyLinearGradient stroke="lavender" color="white" 
           edgeColor="rgba(0,0,0,0)" padding="3" >
           <MyHeader2 text="CONTACT" />
